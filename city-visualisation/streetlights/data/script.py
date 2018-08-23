c219bbcde9c348c676d9f4ec5d80f325fd545f05
Regina Pramesti <regina.pramesti.8@gmail.com>
2018-08-23 12:41:56 +1000

    more work on vis

--- city-visualisation/streetlights/data/script.py
+++ city-visualisation/streetlights/data/script.py
@@ -6,19 +6,23 @@ with open('lights.csv', 'r') as source_file:
         new_data_writer = csv.writer(dest_file)
         new_data = []
         count = 0
+        outreach = None
         for row in data:
             # take the combined coordinates and split into lat and long
             coords = row[-1][1:-1].split(',')
+            outreach = row[-3].split(' ')
 
             del row[-1]
             new_row = row
 
             if count == 0:
-                new_row.append('Latitude')
-                new_row.append('Longitude')
+                new_row.append('latitude')
+                new_row.append('longitude')
                 new_data_writer.writerow(new_row)
                 count += 1 # workaround for errors with changing geolocation to 2 columns
             else:
+                if outreach[0] != 'NONE':
+                    new_row[-2] = float(outreach[0])
                 new_row.append(coords[0])
                 new_row.append(coords[1])
                 new_data_writer.writerow(new_row)
